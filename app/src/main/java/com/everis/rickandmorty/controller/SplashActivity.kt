58e813ae259e8e65ed2e07d887674310802081da
omontejo <omontejo@everis.com>
2021-04-16 10:03:15 -0500

    Splash activity

---
+++ app/src/main/java/com/everis/rickandmorty/controller/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.everis.rickandmorty.controller
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.everis.rickandmorty.databinding.ActivityEpisodeDetailBinding
+import com.everis.rickandmorty.databinding.ActivityRickandmortyBinding
+import com.everis.rickandmorty.databinding.SplashActivityBinding
+
+class SplashActivity: AppCompatActivity() {
+    private lateinit var binding: SplashActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SplashActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,RickAndMortyActivity::class.java))
+
+        }, 3000)
+
+
+
+    }
+}
